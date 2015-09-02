b26d19cb98d47f4fcba9a83e52d2dde22c6c0a0a
Harper Jiang <harperjiang@msn.com>
2015-09-02 02:34:21 -0500

    help content

---
+++ calendar/src/main/webapp/code/help_topic.js
@@ -0,0 +1,104 @@
+otms.namespace('otms.helpPage');
+
+otms.helpPage.topicRoot = {
+	"children" : [ {
+		"id" : 1,
+		"topic" : "课程相关",
+		"children" : [ {
+			"id" : 101,
+			'topic' : '试听课',
+			'children' : [ {
+				"id" : 10101,
+				'topic' : '如何预约试听课？'
+			}, {
+				"id" : 10102,
+				'topic' : '如何查看已预约的试听课？'
+			}, {
+				"id" : 10103,
+				'topic' : '我可以取消预约的试听课吗？'
+			}, {
+				"id" : 10104,
+				'topic' : '我错过了试听课的时间怎么办？'
+			}    ]
+		}, {
+			"id" : 102,
+			'topic' : '课程安排',
+			'children' : [ {
+				"id" : 10201,
+				'topic' : '如何查看已预约的课程？',
+			}, {
+				"id" : 10202,
+				'topic' : '我可以修改预约的课程时间吗？',
+			}, {
+				"id" : 10203,
+				'topic' : '我错过了课程时间怎么办？',
+			}  ]
+		} ]
+	}, {
+		"id" : 4,
+		"topic" : "教师相关",
+		"children" : [ {
+			'id' : 401,
+			'topic' : '',
+			'children' : ''
+		}, {
+			'id' : 402,
+			'topic' : '',
+			'children' : ''
+		}, {
+			'id' : 403,
+			'topic' : '',
+			'children' : ''
+		}, {
+			'id' : 404,
+			'topic' : '',
+			'children' : ''
+		}, {
+			'id' : 405,
+			'topic' : '',
+			'children' : ''
+		} ]
+	}, {
+		"id" : 2,
+		"topic" : "软件使用",
+		"children" : [ {
+			'id' : 200,
+			'topic':'网络课堂使用'
+		}, , {
+			'topic' : '',
+			'children' : ''
+		}, {
+			'topic' : '',
+			'children' : ''
+		}, {
+			'topic' : '',
+			'children' : ''
+		}, {
+			'topic' : '',
+			'children' : ''
+		} ]
+	}, {
+		"id" : 3,
+		"topic" : "财务相关",
+		"children" : [ {
+			'topic' : '',
+			'children' : ''
+		}, {
+			'topic' : '',
+			'children' : ''
+		}, {
+			'topic' : '',
+			'children' : ''
+		}, {
+			'topic' : '',
+			'children' : ''
+		}, {
+			'topic' : '',
+			'children' : ''
+		} ]
+	} ]
+};
+
+otms.helpPage.helpContent = {};
+
+otms.helpPage.popularTopic = [];
