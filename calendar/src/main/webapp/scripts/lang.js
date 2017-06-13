4faef7b2b54e306a9b63ee54778b56544c0c1dc3
Hao Jiang <harperjiang@msn.com>
2017-06-13 02:35:08 -0500

    Migrating to Bootstrap

--- calendar/src/main/webapp/scripts/lang.js
+++ calendar/src/main/webapp/scripts/lang.js
@@ -1,27 +1,48 @@
-otms.namespace('otms.lang');
-
-otms.lang.setLang = function(lang) {
-	// The language setting is stored in local storage
+function requestWithLang(lang) {
+	// A flag for pages that cannot be executed twice
+	otms.headerPage.needReload = true;
 	localStorage.setItem("otms.lang", lang);
+	var locstring = window.location.toString().split('?');
+	var url = locstring[0];
+	if (locstring.length == 2) {
+		// keep the parameter
+		var params = locstring[1].split('&');
+		var paramobj = {};
+		for ( var index in params) {
+			var param = params[index].split('=');
+			paramobj[param[0]] = param[1];
+		}
+		paramobj['lang'] = lang;
+		window.location = url + '?' + $.param(paramobj);
+	} else {
+		window.location = url + "?lang=" + lang;
+	}
 };
 
-otms.lang.load = function(key) {
+function changeLang(item) {
+	var lang = $(item).val();
+	requestWithLang(lang);
+};
+
+function handleLang() {
+	// Set the language to be selected
 	var lang = localStorage.getItem('otms.lang');
+	var serverLang = $('meta[name=lang]').attr("content");
+
 	if (otms.isEmpty(lang)) {
-		lang = 'default';
+		lang = 'en_US';
 	}
-	if (otms.lang[lang] !== undefined) {
-		return otms.lang[lang][key];
+	if (otms.isEmpty(serverLang)) {
+		serverLang = 'en_US';
 	}
 
-	var url = "../lang/lang_{0}.json"
-	url = otms.FormatUtil.format(url, lang);
+	if (lang != serverLang) {
+		// Always use local lang, Resend current request
+		requestWithLang(lang);
+		return;
+	}
 
-	$.ajax(url, {
-		'async' : false,
-		'complete' : function(data) {
-			debugger;
-			
-		}
-	});
+	if (!otms.isEmpty(lang) && $('#language_select').val() != lang) {
+		$("#language_select").val(lang);
+	}
 };
